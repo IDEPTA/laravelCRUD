6d4e7d7219e856733ce55c069226317d179956da
sergey <procopchuk14092002@yandex.ru>
2024-02-01 21:31:55 +0300

    read

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,9 +26,6 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
         ];
     }
 
@@ -37,8 +34,6 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,28 @@
 @extends('layout')
 @section('title')
-    <h1>Users</h1>
+    <h1>Usersg</h1>
 @endsection
 @section('content')
 <table class="table">
     <thead>
       <tr>
         <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">Name</th>
+        <th scope="col">Emal</th>
+        <th scope="col">created</th>
+        <th scope="col">updated</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
+        @foreach ($data as $item)
+            <tr>
+                <td>{{$item->id}}</td>
+                <td>{{$item->name}}</td>
+                <td>{{$item->email}}</td>
+                <td>{{$item->created_at}}</td>
+                <td>{{$item->updated_at}}</td>
+            </tr>
+        @endforeach
     </tbody>
   </table>
 @endsection

6fae05eed1bdff5b6b1e1ef926e36e21b7f1c85d
sergey <procopchuk14092002@yandex.ru>
2024-02-01 20:58:54 +0300

    controller and router

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UsersController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('users');
 });
+Route::resource('users', UsersController::class);
